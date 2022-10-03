ff0eee18b6271095ac7472c86aed06d897aac608
SimonChabrier <simonchabrier@gmail.com>
2022-10-03 23:57:07 +0200

    add js ans data objects

---
+++ assets/js/data.js
@@ -0,0 +1,55 @@
+const data = [
+    {
+        id : 'id',
+        title : 'titre du projet',
+        date : 'date du projet',
+        techno : 
+            [{
+                1 : 'symfo', 
+                2 : 'css'
+            }],
+        description : 'projet short description text',
+        git : 'githuburl',
+        web: 'weburlvalue',
+    },
+    {
+        id : 'id',
+        title : 'titre du projet',
+        date : 'date du projet',
+        techno : 
+            [{
+                1 : 'symfo', 
+                2 : 'css'
+            }],
+        description : 'projet short description text',
+        git : 'githuburl',
+        web: 'weburlvalue',
+    },
+    {
+        id : 'id',
+        title : 'titre du projet',
+        date : 'date du projet',
+        techno : 
+            [{
+                1 : 'symfo', 
+                2 : 'css'
+            }],
+        description : 'projet short description text',
+        git : 'githuburl',
+        web: 'weburlvalue',
+    },
+    {
+        id : 'id',
+        title : 'titre du projet',
+        date : 'date du projet',
+        techno : 
+            [{
+                1 : 'symfo', 
+                2 : 'css'
+            }],
+        description : 'projet short description text',
+        git : 'githuburl',
+        web: 'weburlvalue',
+    },
+]
+
