a1f2a709a9c5752dc29f85d6572a081eff782a9d
Simon Chabrier <simonchabrier@gmail.com>
2022-10-10 07:37:41 +0200

    add first project

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,70 +1,21 @@
 const data = [
     {
-        id : 'id',
-        title : 'titre du projet 1',
-        picture : 'https://picsum.photos/250',
-        date : 'date du projet',
+        id : '1',
+        title : 'O\'Programme',
+        picture : './assets/pictures/project/oprogramme.webp',
+        date : 'Octobre 2022',
         techno : 
             [{
-                1 : 'symfo', 
-                2 : 'css'
+                1 : 'PHP', 
+                2 : 'HTML',
+                3 : 'CSS',
+                4 : 'Bootstrap',
             }],
-        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit Ipsum suscipit cupiditate omnis excepturi dignissimos amet iste" ,
+        description : "Simple MVC framework made with PHP, HTML and CSS. User can register, login, logout, create, read, update and delete posts. This project was made for my studies.",
         web : 
         [{
-            1 : 'github.com', 
-            2 : 'www.simon.com'
-        }],
-    },
-    {
-        id : 'id',
-        title : 'titre du projet 2',
-        picture : 'https://picsum.photos/250',
-        date : 'date du projet',
-        techno : 
-            [{
-                1 : 'symfo', 
-                2 : 'css'
-            }],
-        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit Ipsum suscipit cupiditate omnis excepturi dignissimos amet iste" ,
-        web : 
-        [{
-            1 : 'github.com', 
-            2 : 'www.simon.com'
-        }],
-    },
-    {
-        id : 'id',
-        title : 'titre du projet 3',
-        picture : 'https://picsum.photos/250',
-        date : 'date du projet',
-        techno : 
-            [{
-                1 : 'symfo', 
-                2 : 'css'
-            }],
-        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit Ipsum suscipit cupiditate omnis excepturi dignissimos amet iste" ,
-        web : 
-        [{
-            1 : 'github.com', 
-            2 : 'www.simon.com'
-        }],
-    },
-    {
-        id : 'id',
-        title : 'titre du projet 4',
-        picture : 'https://picsum.photos/250',
-        date : 'date du projet',
-        techno : 
-            [{
-                1 : 'symfo', 
-                2 : 'css'
-            }],
-        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit Ipsum suscipit cupiditate omnis excepturi dignissimos amet iste" ,
-        web : 
-        [{
-            1 : 'github.com', 
-            2 : 'www.simon.com'
+            1 : '', 
+            2 : '',
         }],
     },
 ]
