11bb60d4723cabdf44a4df923ca4e4a2f97f7464
Simon Chabrier <simonchabrier@gmail.com>
2022-10-20 13:03:16 +0200

    add oplaystore

--- assets/js/data.js
+++ assets/js/data.js
@@ -10,6 +10,7 @@ const data = [
                 2 : 'HTML',
                 3 : 'CSS',
                 4 : 'Bootstrap5',
+                5 : 'Alto Router',
             }],
         description : "Simple MVC app. User can register, login, logout, create, read, update and delete posts. This project was made for my studies to learn PHP fondamentals.",
         web : 
@@ -20,9 +21,9 @@ const data = [
     },
 
     {
-        id : '1',
-        title : 'O\'Programme',
-        picture : './assets/pictures/project/oprogramme.webp',
+        id : '2',
+        title : 'O\'Playstore',
+        picture : './assets/pictures/project/oplaystore.webp',
         date : 'Octobre 2022',
         techno : 
             [{
@@ -30,31 +31,13 @@ const data = [
                 2 : 'HTML',
                 3 : 'CSS',
                 4 : 'Bootstrap5',
+                5 : 'Plates',
+                6 : 'Symfony Router',
             }],
-        description : "Simple MVC app. User can register, login, logout, create, read, update and delete posts. This project was made for my studies to learn PHP fondamentals.",
+        description : "Simple MVC app. This project was made for my studies to learn PHP fondamentals. Use Plates for templating and Symfony Router for routing.",
         web : 
         [{
-            'GitHub' : 'https://github.com/SimonChabrier/Basic_mvc_php', 
-            'Http' : null,
-        }],
-    },
-
-    {
-        id : '1',
-        title : 'O\'Programme',
-        picture : './assets/pictures/project/oprogramme.webp',
-        date : 'Octobre 2022',
-        techno : 
-            [{
-                1 : 'PHP', 
-                2 : 'HTML',
-                3 : 'CSS',
-                4 : 'Bootstrap5',
-            }],
-        description : "Simple MVC app. User can register, login, logout, create, read, update and delete posts. This project was made for my studies to learn PHP fondamentals.",
-        web : 
-        [{
-            'GitHub' : 'https://github.com/SimonChabrier/Basic_mvc_php', 
+            'GitHub' : 'https://github.com/SimonChabrier/mvc_using_plates', 
             'Http' : null,
         }],
     },
