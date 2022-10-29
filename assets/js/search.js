ce7681a9367af5754bd9134498f64ec1bd4efa09
Simon Chabrier <simonchabrier@gmail.com>
2022-10-30 01:00:04 +0200

    add buttons tags list

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,47 +2,49 @@ const search = {
 
 init: function() 
 {   
-    search.searchInTechnoTags();
+    search.inputSearchInTechnoTags();
     console.log('search init');
 },
 
-searchInTechnoTags: function() 
+inputSearchInTechnoTags: function() 
 {   
     const searchInput = document.getElementById('search_input');
-    
-    searchInput.addEventListener('keyup', (e) => {
-    let search_value = e.target.value;
-    search.dislayResults(search_value);
+    searchInput.addEventListener('keyup', (event) => {
+        event.target.value.length >= 3 ? card.resetCards() + card.resetCountMessage() + search.dislayResults(event.target.value) : card.resetCards() + card.resetCountMessage() + card.constructCards(data);
     });  
 },    
 
 dislayResults: function(search_value) 
 {   
     let results = [];
-
+   
     projet_techno = data.forEach(project => { project
         project.techno.forEach(techno => {
             for (let key in techno) 
-            { 
-                if(techno[key].toLowerCase() == (search_value.toLowerCase()))
-                {
+            {   
+                if(techno[key].toLowerCase().includes(search_value.toLowerCase()))
+                {      
                     results.push(project);
                 }
             } 
         });
     });
 
-    card.resetCards();
-    card.resetCountMessage();
-    card.fetchData(results);
-
-    if (search_value == ""){
-        card.resetCards();
-        card.resetCountMessage();
-        card.fetchData(data);
-    }   
+    card.constructCards(results, search_value);
 },   
 
+inputReset: function() 
+{
+    const resetButton = document.getElementById("reset_btn");
+    const searchInput = document.getElementById("search_input");
+    
+    resetButton.addEventListener("click", () => {
+        searchInput.value = "";
+        card.resetCards();    
+        card.constructCards(data);
+    });
+
+},
 
 
 }
