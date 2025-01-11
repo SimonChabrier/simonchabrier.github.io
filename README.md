1564c1262bae7ef007397a3d830763caee4928f3
Simon Chabrier <simonchabrier@gmail.com>
2025-01-11 23:23:34 +0100

    ok fonctionnel

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 Cv github page avec Astro 
 
 - thème utilisé : https://github.com/Smilesharks/dev-portfolio
+- tuto https://www.youtube.com/watch?v=4poD0GjnYXo&t=1687s
 
 - `pnpm dev` pour lancer le serveur de développement
+- `pnpm build` pour générer le site statique
+
+- Création du dossier .github/workflows pour le déploiement automatique sur github pages...
--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://simonchabrier.github.io/', // URL du site
-  base: '/', // racine du repository, car il s'agit d'un site personnel
+  base: '/', // racine du repository, car il n'y a pas de sous-dossier pour ce projet il est à la racine
   trailingSlash: 'always', // Pour que toutes les URLs se terminent par un slash
   integrations: [tailwind()],
 });
