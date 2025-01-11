e0b88024437c6e687fd293435611669f79cce633
Simon Chabrier <simonchabrier@gmail.com>
2025-01-11 22:02:33 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://simonchabrier.github.io/', // url du site
+  base: 'simonchabrier.github.io', // nom du repo
+  trailingSlash:'always', // pour les liens relatifs
   integrations: [tailwind()],
 });
