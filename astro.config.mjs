f8dd8c269ad4b1d9509d88acc98bd14b058a404f
Simon Chabrier <simonchabrier@gmail.com>
2025-01-15 07:08:19 +0100

    add castor project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://simonchabrier.github.io/', // URL du site
+  site: 'https://simonchabrier.github.io', // URL du site
   base: '/', // racine du repository, car il n'y a pas de sous-dossier pour ce projet il est à la racine
   trailingSlash: 'always', // Pour que toutes les URLs se terminent par un slash
   integrations: [tailwind()],
