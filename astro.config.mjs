f311656aeed142d9f562b08faec21626d8ecc637
Simon Chabrier <simonchabrier@yahoo.fr>
2025-01-21 19:13:38 +0100

    update matomo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import matomo from "@jop-software/astro-matomo";
 
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://simonchabrier.github.io", // URL principale
