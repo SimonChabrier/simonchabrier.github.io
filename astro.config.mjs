e0dc93577a04494f962cd73b49d151023c443caa
Simon Chabrier <simonchabrier@yahoo.fr>
2025-01-21 06:17:11 +0100

    update matomo config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,21 @@ import matomo from "@jop-software/astro-matomo";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://simonchabrier.github.io", // URL du site
+  site: "https://simonchabrier.github.io", // URL principale
   base: "/", // Racine du repository
   trailingSlash: "always", // Toutes les URLs se terminent par un slash
   integrations: [
     tailwind(),
     matomo({
-      baseUrl: "https://matomo.simschab.cloud/", // URL de ton Matomo
-      siteId: 1, // ID du site Matomo
+      baseUrl: "https://matomo.simschab.cloud/", // URL de ton serveur Matomo
+      siteId: 1, // ID du site Matomo (le même pour tous les domaines)
+      jsTrackerUrl: "https://matomo.simschab.cloud/matomo.js", // Tracker JS pour tous les domaines
+      phpTrackerUrl: "https://matomo.simschab.cloud/matomo.php", // URL du tracker PHP
+      trackerDomains: [
+        "simschab.cloud",
+        "simonchabrier.fr",
+        "simonchabrier.com",
+      ],
     }),
   ],
 });
