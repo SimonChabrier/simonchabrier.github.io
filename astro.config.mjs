99ecf9c13a21818559dfa310439155c432fe6cd4
Simon Chabrier <simonchabrier@yahoo.fr>
2025-01-21 06:14:52 +0100

    add matotomo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import matomo from "@jop-software/astro-matomo";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://simonchabrier.github.io', // URL du site
-  base: '/', // racine du repository, car il n'y a pas de sous-dossier pour ce projet il est à la racine
-  trailingSlash: 'always', // Pour que toutes les URLs se terminent par un slash
-  integrations: [tailwind()],
+  site: "https://simonchabrier.github.io", // URL du site
+  base: "/", // Racine du repository
+  trailingSlash: "always", // Toutes les URLs se terminent par un slash
+  integrations: [
+    tailwind(),
+    matomo({
+      baseUrl: "https://matomo.simschab.cloud/", // URL de ton Matomo
+      siteId: 1, // ID du site Matomo
+    }),
+  ],
 });
