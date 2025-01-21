efbed0d93af523e5d32865f7b8fbad9eb47f617b
Simon Chabrier <simonchabrier@yahoo.fr>
2025-01-21 19:36:30 +0100

    update and clean packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import matomo from "@jop-software/astro-matomo";
+// import matomo from "@jop-software/astro-matomo";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,16 +9,16 @@ export default defineConfig({
   trailingSlash: "always", // Toutes les URLs se terminent par un slash
   integrations: [
     tailwind(),
-    matomo({
-      baseUrl: "https://matomo.simschab.cloud/", // URL de ton serveur Matomo
-      siteId: 1, // ID du site Matomo (le même pour tous les domaines)
-      jsTrackerUrl: "https://matomo.simschab.cloud/matomo.js", // Tracker JS pour tous les domaines
-      phpTrackerUrl: "https://matomo.simschab.cloud/matomo.php", // URL du tracker PHP
-      trackerDomains: [
-        "simschab.cloud",
-        "simonchabrier.fr",
-        "simonchabrier.com",
-      ],
-    }),
+    // matomo({
+    //   baseUrl: "https://matomo.simschab.cloud/", // URL de ton serveur Matomo
+    //   siteId: 1, // ID du site Matomo (le même pour tous les domaines)
+    //   jsTrackerUrl: "https://matomo.simschab.cloud/matomo.js", // Tracker JS pour tous les domaines
+    //   phpTrackerUrl: "https://matomo.simschab.cloud/matomo.php", // URL du tracker PHP
+    //   trackerDomains: [
+    //     "simschab.cloud",
+    //     "simonchabrier.fr",
+    //     "simonchabrier.com",
+    //   ],
+    // }),
   ],
 });
