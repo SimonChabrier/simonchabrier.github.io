02c19892d0f11b907742daee95646ae2fdee4aa6
Simon Chabrier <simonchabrier@gmail.com>
2025-02-21 17:54:56 +0100

    update meta robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://simonchabrier.github.io", // URL principale
+  // site: "https://simonchabrier.github.io", // URL principale
+  site: isGitHubPages ? "https://simonchabrier.github.io" : undefined, // URL principale
   base: "/", // Racine du repository
   trailingSlash: "always", // Toutes les URLs se terminent par un slash
   integrations: [
